ecf5b591311f48664452197d7c2584a00cebf9e2
Noxi-PC <noxi@example.com>
2025-11-19 17:21:29 -0300

    fix: Resolver error de router no definido en home.js

--- next-app/pages/home.js
+++ next-app/pages/home.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
   const [hasChecked, setHasChecked] = useState(false);
 
