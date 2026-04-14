c4e671a491f9292571ded302e0eb3f82032098d0
Auto Commit <auto@local>
2026-04-14 01:31:31 -0300

    fix: migrate Google Fonts to next/font/google to eliminate preload warning

--- next-app/pages/_document.js
+++ next-app/pages/_document.js
@@ -3,23 +3,7 @@
 const Document = () => {
   return (
     <Html lang="es">
-      <Head>
-        {/* Preconnect to Google Fonts and preload the stylesheet correctly to avoid preload warnings */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="preload"
-          as="style"
-          onLoad="this.onload=null;this.rel='stylesheet'"
-        />
-        <noscript>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </noscript>
-      </Head>
+      <Head />
       <body data-theme="dark">
         <Main />
         <NextScript />
